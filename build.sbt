bc99a89d8d0592e3b91f5cec180259fc74a3562e
fboldin <fboldin@twilio.com>
2020-12-31 12:25:22 +0200

    Initial Commit

---
+++ build.sbt
@@ -0,0 +1,17 @@
+name := "StockStreaming"
+
+version := "0.1"
+
+scalaVersion := "2.12.7"
+
+val AkkaVersion = "2.6.8"
+val AkkaHttpVersion = "10.2.2"
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-actor-typed" % AkkaVersion,
+  "com.typesafe.akka" %% "akka-stream" % AkkaVersion,
+  "com.typesafe.akka" %% "akka-http" % AkkaHttpVersion,
+  "de.heikoseeberger" %% "akka-http-circe" % "1.35.2",
+  "com.typesafe.akka" %% "akka-http-spray-json" % AkkaHttpVersion,
+  "io.circe" %% "circe-core" % "0.13.0"
+
+)
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,37 @@
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
+import akka.http.scaladsl.unmarshalling.{Unmarshal, Unmarshaller}
+
+import scala.concurrent.Future
+import scala.util.{Failure, Success}
+
+object Main {
+
+  // needed to run the route
+  implicit val system = ActorSystem(Behaviors.empty, "SprayExample")
+  // needed for the future map/flatmap in the end and future in fetchItem and saveOrder
+  implicit val executionContext = system.executionContext
+  implicit val um = Unmarshaller[HttpResponse, Stock]
+  // domain model
+  final case class Stock(
+    lastUpdated: String,
+    symbol: String,
+    targetHigh: Long,
+    targetLow: Long,
+    targetMean: Long,
+    targetMedian: Long
+  )
+
+  def main(args: Array[String]): Unit = {
+    val responseFuture: Future[HttpResponse] = Http().singleRequest(HttpRequest(
+      uri = "https://finnhub.io/api/v1/stock/price-target?symbol=NFLX&token=bvlfk0748v6qdeqd82r0"))
+
+    responseFuture
+      .onComplete {
+        case Success(res) => Unmarshal(res).to[Stock]
+        case Failure(_)   => sys.error("something wrong")
+      }
+  }
+}
